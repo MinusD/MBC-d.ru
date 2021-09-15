4410bfbd95ea4b6554a90879c46693ea3f2f8922
Николай <kkudlinkov@mail.ru>
2021-09-16 00:45:51 +0300

    Добавлен начальный дизайн страницы /team

--- resources/views/livewire/landing/team.blade.php
+++ resources/views/livewire/landing/team.blade.php
@@ -1,6 +1,22 @@
 <div>
-    <div class="bg-gradient-to-br from-indigo-900 to-green-900 min-h-screen overflow-auto">
-
-
+    <div class="flex justify-center bg-gradient-to-br from-indigo-900 to-green-900 min-h-screen overflow-auto">
+        <div class="text-center w-full">
+            <p class="text-white text-4xl pt-16 pb-16 ">Командный состав</p>
+        <div class="max-w-xs mx-auto px-16">
+            <div
+                class="max-w-xs  mx-auto bg-white border-2 pt-4 bg-indigo-900 bg-opacity-40 shadow-xl gap-5 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700">
+                <div class="">
+                    <div class="mx-auto rounded-full border-black h-32 w-32 border overflow-hidden pb-4">
+                        <img class=""
+                             src="https://cdn.discordapp.com/attachments/882642110153637919/887785148626661396/LogoEmpire2.png">
+                    </div>
+                    <span class="text-2xl text-gray-100 my-10">MinusD</span>
+                </div>
+                <span class="text-xl text-gray-100">Founder</span>
+                <p class="text-base text-gray-100 my-2">Back-end developer</p>
+            </div>
+        </div>
+        </div>
     </div>
 </div>
+
