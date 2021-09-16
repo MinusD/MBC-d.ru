3309148f19b68a22135923b0899ddb0390988bb7
Николай <kkudlinkov@mail.ru>
2021-09-16 12:41:26 +0300

    Добавлен первый блок с участниками в /team

--- resources/views/livewire/landing/team.blade.php
+++ resources/views/livewire/landing/team.blade.php
@@ -1,21 +1,58 @@
 <div>
+
     <div class="flex justify-center bg-gradient-to-br from-indigo-900 to-green-900 min-h-screen overflow-auto">
         <div class="text-center w-full">
-            <p class="text-white text-4xl pt-16 pb-16 ">Командный состав</p>
+            <p class="text-white text-4xl pt-16 pb-16 ">Главный директор</p>
         <div class="max-w-xs mx-auto px-16">
             <div
                 class="max-w-xs  mx-auto bg-white border-2 pt-4 bg-indigo-900 bg-opacity-40 shadow-xl gap-5 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700">
-                <div class="">
-                    <div class="mx-auto rounded-full border-black h-32 w-32 border overflow-hidden pb-4">
-                        <img class=""
+                <div class="pb-2 border-double border-b-2 border-light-blue-500">
+
+                    {{-- Блок с изображением--}}
+                    <div class="mx-auto rounded-full border-0 h-32 w-32 overflow-hidden">
+                        <img
                              src="https://cdn.discordapp.com/attachments/882642110153637919/887785148626661396/LogoEmpire2.png">
                     </div>
-                    <span class="text-2xl text-gray-100 my-10">MinusD</span>
+                    {{--Блок с именем и должностью--}}
+                    <span class="text-2xl text-gray-100">MinusD</span>
+
                 </div>
-                <span class="text-xl text-gray-100">Founder</span>
-                <p class="text-base text-gray-100 my-2">Back-end developer</p>
+                    <span class="text-xl text-gray-100">Founder</span>
+                    <p class="text-base text-gray-100 my-2">Back-end developer</p>
+                 </div>
+             </div>
+
+            {{--Блок с участниками--}}
+            <div class="relative pt-16 pb-8 text-center">
+                <span class="text-3xl text-gray-100">Участники</span>
             </div>
-        </div>
+
+
+
+            <div class="max-w-xs mx-auto px-16">
+                <div
+                    class="max-w-xs  mx-auto bg-white border-2 pt-4 bg-indigo-900 bg-opacity-40 shadow-xl gap-5 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700">
+                    <div class="pb-2 border-double border-b-2 border-light-blue-500">
+                        {{-- Блок с изображением--}}
+                        <div class="mx-auto rounded-full h-32 w-32 overflow-hidden">
+                            <img
+                                src="https://cdn.discordapp.com/attachments/765261897716334633/887996167265341450/vFMhT2iCkV0.jpg">
+                        </div>
+                        {{--Блок с именем и должностью--}}
+                        <span class="text-2xl text-gray-100">kkudlinkov</span>
+                    </div>
+                    <span class="text-xl text-gray-100">Moderator</span>
+                    <p class="text-base text-gray-100 my-2">Front-end developer</p>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
         </div>
     </div>
 </div>
