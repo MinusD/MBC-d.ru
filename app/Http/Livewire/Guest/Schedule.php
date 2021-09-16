dcea429e100899eddbe09b8d1e066044081dad9c
MinusD <50549999+MinusD@users.noreply.github.com>
2021-09-17 00:30:20 +0300

    Создано общее расписание

--- app/Http/Livewire/Guest/Schedule.php
+++ app/Http/Livewire/Guest/Schedule.php
@@ -4,38 +4,114 @@ namespace App\Http\Livewire\Guest;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Cookie;
+use WireUi\Traits\Actions;
 
 class Schedule extends Component
 {
+    use Actions;
 
-    public $select_group_module = false;
     public $view_groups;
 
+    public $modal_set = false;
+    public $group_name = '';
+    public $modal_group_name;
+    public $search_error = false;
+    public $lessons_time;
+    public $current_week;
+    public $lessons = [];
 
     protected $groups_list = [];
 
-    public function mount()
+
+    public function openSetModal()
     {
-        if (Cookie::has('schedule-group-name')) {
-            $this->get_data(Cookie::get('schedule-group-name'));
+        if($this->modal_set){
+            $this->modal_set = false;
+        }
+        $this->modal_set = true;
+    }
+
+    public function save()
+    {
+        $this->get_groups();
+        $tmp = $this->groups_list;
+        $key = array_search(
+            $this->modal_group_name,
+            array_column($tmp, 'groupName')
+        );
+        if ($this->groups_list[$key]['groupName'] != $this->modal_group_name) {
+            $this->search_error = true;
         } else {
-            $this->select_group_module = true;
-
-            $path = env('API_SERVER') . 'groups/all';
-
-            $this->groups_list = json_decode(file_get_contents($path));
-            $this->view_groups[0] = $this->groups_list[0];
-            $this->view_groups[1] = $this->groups_list[1];
-            $this->view_groups[2] = $this->groups_list[2];
-            $this->view_groups[3] = $this->groups_list[3];
-            $this->view_groups[4] = $this->groups_list[4];
-//            dd($this->group_list);
+            $this->search_error = false;
+            $this->modal_set = false;
+            $this->group_name = $this->groups_list[$key]['groupName'];
+            Cookie::queue(Cookie::forever('schedule-group-name', $this->group_name));
+            $this->get_group_data();
         }
     }
 
-    public function get_data()
+    public function get_group_data()
     {
+        $path = env('API_SERVER') . 'groups/certain?name=' . urlencode($this->group_name);
+        $path2 = env('API_SERVER') . 'time/week';
 
+        $timetable = json_decode(file_get_contents($path), true)[0];
+        $this->current_week = json_decode(file_get_contents($path2), true);
+
+        $this->lessons_time = $timetable['lessonsTimes'][0];
+
+        if ($this->current_week % 2 == 1) {
+            $c = 'odd';
+        } else {
+            $c = 'even';
+        }
+        foreach ($timetable['schedule'] as $key => $day) {
+            $d = ucfirst($day['day']);
+            $day = $day[$c];
+            $data = array();
+            foreach ($day as $key2 => $para) {
+                foreach ($para as $key3 => $lesson) {
+//                    if (isset($lesson['weeks'])) {
+                    if (is_null($lesson['weeks']) or in_array($this->current_week, $lesson['weeks'])) {
+                        array_push($data, ['n' => $key2, 'name' => $lesson['name'], 'tuter' => $lesson['tutor'],
+                            'place' => $lesson['place'], 'type' => $lesson['type']]);
+//                        echo $d . ' ' . var_dump($lesson) . ' ';
+                    }
+                }
+                if ($key2 == 5 and !isset($p_list['day_name'])) {
+                    array_push($this->lessons, ['day_name' => $d, 'data' => $data]);
+                    unset($data);
+
+                }
+
+            }
+        }
+    }
+
+    public function get_groups()
+    {
+        if (!isset($this->groups_list[1])) {
+            $path = env('API_SERVER') . 'groups/all';
+            $this->groups_list = json_decode(file_get_contents($path), true);
+        }
+    }
+
+    public function mount()
+    {
+//        dd(Cookie::get());
+        if (Cookie::has('schedule-group-name')) {
+            $this->group_name =  Cookie::get('schedule-group-name');
+            $this->modal_group_name =  $this->group_name;
+            $this->get_group_data();
+        } else {
+            $this->modal_set = true;
+//            $this->view_groups[0] = $this->groups_list[0];
+//            $this->view_groups[1] = $this->groups_list[1];
+//            $this->view_groups[2] = $this->groups_list[2];
+//            $this->view_groups[3] = $this->groups_list[3];
+//            $this->view_groups[4] = $this->groups_list[4];
+//            dd($this->group_list);
+        }
     }
 
     public function select($name)
--- resources/views/livewire/guest/schedule.blade.php
+++ resources/views/livewire/guest/schedule.blade.php
@@ -1,3 +1,206 @@
-<div>
-    {{ dd($view_groups) }}
+<div class="bg-gradient-to-br from-indigo-900 to-green-900 min-h-screen overflow-auto">
+
+
+    <div class="px-10 mt-4">
+        <x-modal.card title="Выбор группы" class="z-50" blur wire:model="modal_set">
+            <div class="grid grid-cols-1  gap-4">
+
+
+                <x-input label="Имя группы" wire:model.lazy="modal_group_name"/>
+                @if($search_error)
+                    <span class="text-red-600">Ошибка поиска группы</span>
+                @endif
+                <p><span class="font-bold">Важно! </span>Название группы должно быть в таком же формате, как и в
+                    расаписании (Например: <code>ИКБО-30-21)</code></p>
+
+            </div>
+
+            <x-slot name="footer">
+                <div class="flex justify-between gap-x-4">
+                    <x-button flat negative/>
+
+                    <div class="flex">
+                        <x-button flat label="Отменить" x-on:click="close"/>
+                        <x-button primary label="Сохранить" wire:click="save"/>
+                    </div>
+                </div>
+            </x-slot>
+        </x-modal.card>
+        <header
+            class="hidden lg:block w-full shadow-lg bg-indigo-900 bg-opacity-40 items-center h-16 rounded-2xl z-40 ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700">
+            <div class="relative z-20 flex flex-col justify-center h-full px-3 mx-auto flex-center">
+                <div class="relative items-center pl-1 flex w-full lg:max-w-68 sm:pr-2 sm:ml-0">
+                    <div class="container relative left-0 z-50 flex w-3/4 h-auto h-full">
+                        <style>
+                            @import url('https://fonts.googleapis.com/css2?family=Otomanopee+One&display=swap');
+                        </style>
+                        <a href="{{ route('landing.home') }}">
+                            <span class="text-3xl dark:text-gray-100 font-semibold text-white ml-3"><span
+                                    class="font-bold" style="font-family: 'Otomanopee One', sans-serif;">MBC</span> Studio</span>
+                        </a>
+                    </div>
+
+                    {{-- ШЕСТЕРЁНКА--}}
+                    <div
+                        class="hidden sm:flex relative p-1  items-center justify-end w-1/4 ml-5 mr-4 sm:mr-0 sm:right-auto">
+                        <div class="hidden sm:flex sm:items-center sm:ml-6">
+                            <div class="relative">
+                                <div class="relative">
+
+                                    <div>
+                                        <button type="button" wire:click="openSetModal"
+                                                class="flex justify-center p-2  transition duration-150 ease-in-out border border-transparent rounded-md text-gray-200 lg:dark:bg-gray-900 dark:text-gray-200 dark:bg-gray-800 hover:bg-gray-50  hover:text-gray-700 dark:focus:bg-gray-700 ">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                                 viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"/>
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{--  Конец шестерёнки--}}
+
+                </div>
+            </div>
+        </header>
+    </div>
+    <div class="mx-2 md:mx-10 my-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 md:gap-4 gap-y-4">
+
+        @forelse($lessons as $les)
+        <div
+            class="block w-full shadow-lg bg-indigo-900 bg-opacity-40 items-center rounded-2xl z-40 ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 p-4 w-full">
+            <div class="flex items-center justify-between mb-3">
+                <div class="flex items-center">
+                    <div class="flex flex-col">
+                        <span class="font-bold text-2xl text-white ml-2 capitalize">{{ $les['day_name'] }}</span>
+                    </div>
+                </div>
+            </div>
+            @forelse($les['data'] as $day)
+                <div
+                    class="bg-indigo-900 bg-opacity-40 ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 rounded-md my-3 px-3 py-2 w-full text-white flex space-x-2 justify-between">
+                    <div
+                        class="flex-initial text-gray-200 text-xs md:text-sm flex items-center justify-center py-1 px-3 font-semibold">
+                        {{ $lessons_time[$day['n']] }}
+                    </div>
+                    <div class="flex-grow"><span class="text-white text-xs md:text-base space-y-1 md:space-y-2">{{ $day['name'] }}{{ ', ' . $day['place'] ?? '' }}</span>
+                        <br>
+                        <span class="text-gray-200 text-xs md:text-sm text-sm underline">{{ $day['tuter'] }}</span>
+                    </div>
+                    <div class="flex-initial order-last flex items-center justify-center">
+                        @if($day['type'] == 'пр')
+                        <button
+                            class="flex-no-shrink bg-green-500 hover:bg-green-500 px-1 md:px-2 bg-opacity-75 ml-4 py-0.5 text-xs shadow-sm hover:shadow-lg tracking-wider border-2 border-green-300 hover:border-green-500 text-white rounded-xl md:rounded-full transition ease-in duration-300">
+                            <span class="font-normal md:font-bold md:uppercase">пр</span>
+                        </button>
+                        @else
+                            <button
+                                class="flex-no-shrink bg-red-500 hover:bg-red-600 px-1 md:px-2 bg-opacity-75 ml-4 py-0.5 text-xs shadow-sm hover:shadow-lg tracking-wider border-2 border-red-400 hover:border-red-500 text-white rounded-xl md:rounded-full transition ease-in duration-300">
+                                <span class="font-normal md:font-bold md:uppercase">лк</span>
+                            </button>
+                        @endif
+                    </div>
+                </div>
+            @empty
+
+            @endforelse
+
+{{--            <div--}}
+{{--                class="bg-indigo-900 bg-opacity-40 ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 rounded-md my-3 px-3 py-2 w-full text-white flex space-x-2 justify-between">--}}
+{{--                <div--}}
+{{--                    class="flex-initial text-gray-200 text-xs md:text-sm flex items-center justify-center py-1 px-3 font-semibold">--}}
+{{--                    14:20 – 15:50--}}
+{{--                </div>--}}
+{{--                <div class="flex-grow"><span--}}
+{{--                        class="text-white text-xs md:text-base space-y-1 md:space-y-2">Физика, Д</span>--}}
+{{--                    <br>--}}
+{{--                    <span class="text-gray-200 text-xs md:text-sm text-sm underline">Сафронов А.А.</span>--}}
+{{--                </div>--}}
+{{--                <div class="flex-initial order-last flex items-center justify-center">--}}
+{{--                    <button--}}
+{{--                        class="flex-no-shrink bg-green-500 hover:bg-green-500 px-1 md:px-2 bg-opacity-75 ml-4 py-0.5 text-xs shadow-sm hover:shadow-lg tracking-wider border-2 border-green-300 hover:border-green-500 text-white rounded-xl md:rounded-full transition ease-in duration-300">--}}
+{{--                        <span class="font-normal md:font-bold md:uppercase">пр</span>--}}
+{{--                    </button>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+        </div>
+        @empty
+
+        @endforelse
+
+{{--        <div--}}
+{{--            class="block w-full shadow-lg bg-indigo-900 bg-opacity-40 items-center rounded-2xl z-40 ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 p-4 w-full">--}}
+{{--            <div class="flex items-center justify-between mb-3">--}}
+{{--                <div class="flex items-center">--}}
+{{--                    <div class="flex flex-col">--}}
+{{--                        <span class="font-bold text-2xl text-white ml-2">Понедельник</span>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div--}}
+{{--                class="bg-indigo-900 bg-opacity-40 ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 rounded-md my-3 px-3 py-2 w-full text-white flex space-x-2 justify-between">--}}
+{{--                <div--}}
+{{--                    class="flex-initial text-gray-200 text-xs md:text-sm flex items-center justify-center py-1 px-3 font-semibold">--}}
+{{--                    10:40 - 12:10--}}
+{{--                </div>--}}
+{{--                <div class="flex-grow"><span class="text-white text-xs md:text-base space-y-1 md:space-y-2">История (история России, всеобщая история), Д</span>--}}
+{{--                    <br>--}}
+{{--                    <span class="text-gray-200 text-xs md:text-sm text-sm underline">Назаров А.А.</span>--}}
+{{--                </div>--}}
+{{--                <div class="flex-initial order-last flex items-center justify-center">--}}
+{{--                    <button--}}
+{{--                        class="flex-no-shrink bg-green-500 hover:bg-green-500 px-1 md:px-2 bg-opacity-75 ml-4 py-0.5 text-xs shadow-sm hover:shadow-lg tracking-wider border-2 border-green-300 hover:border-green-500 text-white rounded-xl md:rounded-full transition ease-in duration-300">--}}
+{{--                        <span class="font-normal md:font-bold md:uppercase">пр</span>--}}
+{{--                    </button>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div--}}
+{{--                class="bg-indigo-900 bg-opacity-40 ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 rounded-md my-3 px-3 py-2 w-full text-white flex space-x-2 justify-between">--}}
+{{--                <div--}}
+{{--                    class="flex-initial text-gray-200 text-xs md:text-sm flex items-center justify-center py-1 px-3 font-semibold">--}}
+{{--                    14:20 – 15:50--}}
+{{--                </div>--}}
+{{--                <div class="flex-grow"><span--}}
+{{--                        class="text-white text-xs md:text-base space-y-1 md:space-y-2">Физика, Д</span>--}}
+{{--                    <br>--}}
+{{--                    <span class="text-gray-200 text-xs md:text-sm text-sm underline">Сафронов А.А.</span>--}}
+{{--                </div>--}}
+{{--                <div class="flex-initial order-last flex items-center justify-center">--}}
+{{--                    <button--}}
+{{--                        class="flex-no-shrink bg-green-500 hover:bg-green-500 px-1 md:px-2 bg-opacity-75 ml-4 py-0.5 text-xs shadow-sm hover:shadow-lg tracking-wider border-2 border-green-300 hover:border-green-500 text-white rounded-xl md:rounded-full transition ease-in duration-300">--}}
+{{--                        <span class="font-normal md:font-bold md:uppercase">пр</span>--}}
+{{--                    </button>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+
+{{--            --}}{{--            <div--}}
+{{--            --}}{{--                class="bg-indigo-900 bg-opacity-40 ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 rounded-md my-3 px-3 py-2 w-full text-white flex space-x-2 justify-between">--}}
+{{--            --}}{{--                <div--}}
+{{--            --}}{{--                    class="flex-initial text-gray-200 text-sm flex items-center justify-center py-1 px-3 font-semibold">--}}
+{{--            --}}{{--                    12:40 – 14:10--}}
+{{--            --}}{{--                </div>--}}
+{{--            --}}{{--                <div class="flex-grow"><span class="text-white">Физика</span>--}}
+{{--            --}}{{--                    <br>--}}
+{{--            --}}{{--                    <span class="text-gray-200 text-sm underline">Сафронов А.А.</span>--}}
+{{--            --}}{{--                </div>--}}
+{{--            --}}{{--                <div class="flex-initial flex items-center justify-center">--}}
+{{--            --}}{{--                    <button--}}
+{{--            --}}{{--                        class="order-last flex-no-shrink bg-green-500 hover:bg-green-500 px-2 bg-opacity-75 ml-4 py-0.5 text-xs shadow-sm hover:shadow-lg font-medium tracking-wider border-2 border-green-300 hover:border-green-500 text-white rounded-full transition ease-in duration-300">--}}
+{{--            --}}{{--                       <span class="font-bold">ПР</span>--}}
+{{--            --}}{{--                    </button>--}}
+{{--            --}}{{--                </div>--}}
+{{--            --}}{{--            </div>--}}
+
+
+{{--        </div>--}}
+
+    </div>
+
+
+    {{--    {{ dd($view_groups) }}--}}
 </div>
