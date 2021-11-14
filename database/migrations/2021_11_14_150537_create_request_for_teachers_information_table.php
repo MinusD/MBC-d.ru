c05a52b0c2484440165af10fc3ce7d4e506f8539
MinusD <50549999+MinusD@users.noreply.github.com>
2021-11-14 15:16:10 +0300

    FEATURE: Добавлены модели запросов на добавление и получение информации о преподавателе

---
+++ database/migrations/2021_11_14_150537_create_request_for_teachers_information_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestForTeachersInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_for_teachers_information', function (Blueprint $table) {
+            $table->id();
+            $table->string('short_name');
+            $table->string('name')->nullable();
+            $table->string('comment')->nullable();
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request_for_teachers_information');
+    }
+}
---
+++ database/migrations/2021_11_14_150641_create_request_for_add_teachers_information_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestForAddTeachersInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_for_add_teachers_information', function (Blueprint $table) {
+            $table->id();
+            $table->string('short_name');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('comment')->nullable();
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request_for_add_teachers_information');
+    }
+}
