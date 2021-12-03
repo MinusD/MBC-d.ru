652a6454b40ab114a57535650741fdbb0aae0f3b
MinusD <50549999+MinusD@users.noreply.github.com>
2021-12-03 13:17:07 +0300

    FEATURE: Добавлена модель кастомной ссылки

---
+++ database/migrations/2021_12_03_130546_create_custom_links_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custom_links', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->default('Без названия');
+            $table->string('full')->nullable();
+            $table->string('short')->nullable();
+            $table->integer('counter')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custom_links');
+    }
+}
